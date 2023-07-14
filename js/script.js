f9d540a876c49218b79bbe3224c259f8463f5fb5
turbobaguettes <kaspersky.kwinta@gmail.com>
2023-07-14 17:56:33 +0200

    mobile menu works

--- js/script.js
+++ js/script.js
@@ -1,14 +1,13 @@
 "use strict";
 
-console.log(123);
-
 const navLinks = document.querySelector(".wrapper-nav");
 const openMenuIcon = document.querySelector(".icon-menu");
 const closeMenuIcon = document.querySelector(".icon-close-menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-openMenuIcon.addEventListener("click", function () {
+mobileMenu.addEventListener("click", function () {
 	console.log("click");
 	navLinks.classList.toggle("visible");
-	openMenuIcon.classList.add("hidden");
+	openMenuIcon.classList.toggle("hidden");
 	closeMenuIcon.classList.toggle("hidden");
 });
